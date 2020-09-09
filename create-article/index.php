02ed5a31d5e5c2aa7c7a2c9c1e378288971d8da3
Natascha <Natascha@siriusschool.be>
2020-09-09 14:24:25 +0200

    folder create-article

---
+++ create-article/index.php
@@ -0,0 +1,19 @@
+<?php require_once "db.php"; ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Un article</title>
+</head>
+
+<body>
+  <a href="create.php">Publier un nouveau article</a>
+  <h1>
+    Le flux des articles
+  </h1>
+</body>
+
+</html>
