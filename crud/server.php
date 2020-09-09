f5d6d308ff80307430123fc9cfdb70d2d4e4e635
Natascha <Natascha@siriusschool.be>
2020-09-09 22:36:55 +0200

    update

--- crud/server.php
+++ crud/server.php
@@ -11,28 +11,28 @@ $db = mysqli_connect('localhost', 'root', "", 'registration');
 
 if (isset($_POST['register'])) {
   $id = mysqli_real_escape_string($db, $_POST['id']);
-  $email = mysqli_real_escape_string($db, $_POST['usermail']);
+  $usermail = mysqli_real_escape_string($db, $_POST['usermail']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 }
 
 // ensure that form fields are ...filled
 
 if (empty($id)) {
-  array_push($erros, "Username is required"); // add error to errors array
+  array_push($errors, "Username is required"); // add error to errors array
 }
 
 if (empty($usermail)) {
-  array_push($erros, "Email is required"); // add error to errors array
+  array_push($errors, "Email is required"); // add error to errors array
 }
 
 if (empty($password)) {
-  array_push($erros, "Password is required"); // add error to errors array
+  array_push($errors, "Password is required"); // add error to errors array
 }
 
 // if there are no errors, save user to database
 if (count($errors) == 0) {
   $password = md5($password);
   $sql = "INSERT INTO users (username, usermail, password)
-  VALUES ('$id', '$email', '$password')";
+  VALUES ('$id', '$usermail', '$password')";
   mysqli_query($db, $sql);
 }
