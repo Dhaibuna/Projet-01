4f955431561f18ea539a88994a77c8afb9198f46
Natascha <Natascha@siriusschool.be>
2020-09-09 22:28:23 +0200

    update

--- crud/login.php
+++ crud/login.php
@@ -1,39 +1,31 @@
-<?php include("server.php"); ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Register</title>
+  <title>Sign up</title>
 </head>
 
 <body>
   <!-- Création d'un formulaire d'enregistrement en HTML -->
   <form action="" method="POST" class="form-example">
-    <!-- Les erreurs vont s'afficher ici -->
-    <?php include('errors.php'); ?>
     <div class="form-example">
       <label for="username"> Username </label>
       <input type="text" name="name" id="username" required>
     </div>
     <div class="form-example">
-      <label for="email"> E-mail </label>
-      <input type="email" name="email" id="email" required>
-    </div>
-    <div class="form-example">
-      <label for="password"> Password </label>
+      <label for=" password"> Password </label>
       <input type="password" name="password" id="password" required>
     </div>
     <div class="form-example">
-      <button type="submit" name="register" class="btn"> Join us </button>
+      <button type="submit" name="login" class="btn"> Sign in </button>
     </div>
     <p>
-      Already within us ? <a href="login.php"> Sign in </a>
+      Not yet within us ? <a href="register.php"> Sign up </a>
     </p>
 
   </form>
 </body>
 
-</html>
+</html
