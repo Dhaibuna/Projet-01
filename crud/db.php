8f5aaf2ff51441a61bb7e38f7a50b9dbbeb6ad0e
Natascha <Natascha@siriusschool.be>
2020-09-10 11:01:05 +0200

    update db file

--- crud/db.php
+++ crud/db.php
@@ -10,5 +10,15 @@ $password = '';
 try {
 
   $connection = new PDO($dsn, $username, $password);
+
+  // On crée la table et ses colonnes
+
+  $sql = "CREATE TABLE users(
+    Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    UserName VARCHAR(30) NOT NULL,
+    UserMail VARCHAR(30) NOT NULL,
+    UserPass VARCHAR(30) NOT NULL,";
+
+  $connection->exec($sql);
 } catch (PDOException $e) {
 }
