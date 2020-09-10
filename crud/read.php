454b38cd874333797694974558b8ff71bb3c92ce
Natascha <Natascha@siriusschool.be>
2020-09-10 17:42:03 +0200

    Update read.php

--- crud/read.php
+++ crud/read.php
@@ -1,7 +1,8 @@
 <?php require('db.php');
 
-$sql = 'SELECT * FROM users';
-
+// On sélectionne tout dans notre base de données.
+$sql = 'SELECT FROM users';
 $statement = $connection->prepare($sql);
+
 // Maintenant on précise par quelle variable de PhP le :email va être remplacée
 $data $statement->execute([":email" => $email]);
