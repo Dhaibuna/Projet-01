f1e66470f2139973d6db56b3be99a8ceb422f912
Yana Snytko <66694075+yanasnytko@users.noreply.github.com>
2020-09-13 00:49:08 +0200

    Delete read.php

--- crud/read.php
+++
@@ -1,4 +0,0 @@
-$sql = "SELECT * FROM users WHERE email=:email";
-$statement = $connection->prepare($sql);
-// Maintenant on précise par quelle variable de PhP le :email va être remplacée
-$data $statement->execute([":email" => $email]);
